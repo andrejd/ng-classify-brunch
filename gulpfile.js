53840b04ece42b378cc8664de83526a24c4f6666
AndrejD <andrejdebeljak@gmail.com>
2015-01-02 13:55:40 +0100

    chore(gulpfile): Add new gulp task for changelog generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp 			= require('gulp');
 var plugins         = require('gulp-load-plugins')();
+var pkg             = require('./package.json');
+var conChangelog    = require('conventional-changelog');
+var fs              = require('fs');
+var gutil           = require('gulp-util');
+
+var CHANGELOG_FILE  = 'CHANGELOG.md';
 
 gulp.task('build', function () {
 
@@ -14,4 +20,17 @@ gulp.task('watch', function() {
 
     gulp.watch(['./src/*.coffee'],['build']);
 
+});
+
+gulp.task('changelog', function(){
+	options = {
+		repository : pkg.repository.url,
+		version    : pkg.version,
+		file       : CHANGELOG_FILE,
+		log        : gutil.log
+	};
+
+	conChangelog(options, function(err, log) {
+		fs.writeFile(CHANGELOG_FILE, log);
+	});
 });
